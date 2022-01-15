b4c111f5d3d189247f3385e35204e42a9caa09e6
amir shahsahebi <shahsahebiamir@gmail.com>
2022-01-15 13:20:27 +0330

    one step completed

---
+++ Day 1/codeWars2.js
@@ -0,0 +1,68 @@
+// Human readable working hours
+
+// Your task in order to complete this Kata is to write a function which formats a working hours schedule, given as an array of objects, in a human-friendly way.
+
+// The function must accept an unsorted array. If the array is empty, it just returns an empty array. Otherwise, it should make a sorted human-friendly schedule of working hours and return it as a string.
+
+// The output format for one day should be SUN: 11:00 - 23:00.
+
+// If two or more days of the week in a row have the same working hours they should be concatenated and have the following format: MON - WED: 11:00 - 23:00.
+
+let readableTimetable = (workdays) => {
+    let arr = []
+    workdays.map(x=> 
+        // arr.push(`${x["day"]}: ${x["from"]} - ${x["to"]}`)
+        // arr.push([x["day"],x["from"]+' '+x["to"]])
+        arr[x["day"]] = x["from"]+' '+x["to"]
+        )
+        let x = arr[0]
+        for (let i = 1 , i<arr.length , i++) {
+            
+        }
+
+    return arr
+  };
+
+  const data = [
+    {
+        "day": "sat",
+        "from": "10:00",
+        "to": "23:00"
+    },
+    {
+        "day": "mon",
+        "from": "11:00",
+        "to": "23:00"
+    },
+    {
+        "day": "tue",
+        "from": "11:00",
+        "to": "23:00"
+    },
+    {
+        "day": "wed",
+        "from": "11:00",
+        "to": "23:00"
+    },
+    {
+        "day": "thu",
+        "from": "12:00",
+        "to": "23:00"
+    },
+    {
+        "day": "fri",
+        "from": "12:00",
+        "to": "23:00"
+    },
+    {
+        "day": "sun",
+        "from": "11:00",
+        "to": "23:00"
+    }
+]
+console.log(readableTimetable(data))
+// Out put
+// MON - WED: 11:00 - 23:00
+// THU - FRI: 12:00 - 23:00
+// SAT: 10:00 - 23:00
+// SUN: 11:00 - 23:00
