346ecd326c1fd59c1d53b7e538352fcffa9ef15e
amir shahsahebi <shahsahebiamir@gmail.com>
2022-01-16 10:14:29 +0330

    arr.length===0 for checking empty arr

--- testEmptyArray.js
+++ testEmptyArray.js
@@ -1,6 +1,6 @@
 let empty1 = []
 // console.log(empty1)
 let isEmptyArray = arr => {
-    if (arr === []) return []
+    if (arr.length===0) return []
 }
-console.log(isEmptyArray([]))
+console.log(isEmptyArray(empty1))
