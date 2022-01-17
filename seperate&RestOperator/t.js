60a0c698134367ebaa1a8bfa880ce0f1bf80ff4e
amir-shahsahebi <92686170+amir-shahsahebi@users.noreply.github.com>
2022-01-17 20:09:44 +0330

    add function in obj

--- seperate&RestOperator/t.js
+++ seperate&RestOperator/t.js
@@ -3,19 +3,26 @@ let obj = {
     "name" : "PC",
     "age" : 58,
     "color" : "black",
-    "price" : "$100",
+    "price value" : "$100",
+    hdd : "2tb",
+    count : x=> {
+        let count
+        count=count+x.map(y=>y)
+    }
 }
-let obj2 = {
-    name : "PC",
-    age : 58,
-    color : "black",
-    price : "$100",
-}
+
+
+// let obj2 = {
+//     name : "PC",
+//     age : 58,
+//     color : "black",
+//     price : "$100",
+// }
 let arr = [1,2,3,4,5,6,7,8,9]
 console.log(obj)
 console.log(arr)
 console.log(...arr)
-let {name , age , color , price} = obj
-console.log(name)
-// let {"name" , age , color , price} = obj2
+let { name, age, color, pricevalue, ...a } = obj;
+console.log(a)
+// let {name , age , color , price} = obj2
 // console.log(name)
