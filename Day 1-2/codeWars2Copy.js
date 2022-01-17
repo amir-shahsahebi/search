eb5af4c3ffb3cfd406d4675bd755e47a27883147
amir shahsahebi <shahsahebiamir@gmail.com>
2022-01-17 13:17:01 +0330

    fixing bugs

--- Day 1-2/codeWars2Copy.js
+++ Day 1-2/codeWars2Copy.js
@@ -70,7 +70,10 @@ let readableTimetable = (workdays) => {
 console.log(arr);
         arr2=arr2.map((x) => (x[1].length === 0 ? (x = null) : x))
         .map((x) =>
-          x[0].length > 9 ? [x[0].slice(0, 3) + x[0].slice(9), x[1]] : x
+          x[0].length === 15 ? [x[0].slice(0, 3) + x[0].slice(9), x[1]] 
+          : x[0].length === 21 ? [x[0].slice(0, 3) + x[0].slice(15), x[1]] 
+          : x[0].length === 27 ? [x[0].slice(0, 3) + x[0].slice(22), x[1]] 
+          : x
         )
         .map((x) => x[0] + ": " + x[1]);
         // .map((x) => x[0] + ": " + x[1]);
