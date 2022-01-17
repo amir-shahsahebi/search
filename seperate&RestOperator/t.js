27b4fc91a9f0fa879e169ef23d841bc0651449e6
amir shahsahebi <shahsahebiamir@gmail.com>
2022-01-17 15:42:04 +0330

    array added

--- seperate&RestOperator/t.js
+++ seperate&RestOperator/t.js
@@ -3,6 +3,17 @@ let obj = {
     "name" : "PC",
     "age" : 58,
     "color" : "black",
-    "price" : "$100"  
+    "price" : "$100",
 }
+let obj2 = {
+    "name" : "PC",
+    "age" : 58,
+    "color" : "black",
+    "price" : "$100",
+}
+let arr = [1,2,3,4,5,6,7,8,9]
 console.log(obj)
+console.log(arr)
+console.log(...arr)
+let {name , age , color , price} = obj
+console.log(name)
