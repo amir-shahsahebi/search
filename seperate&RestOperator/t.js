8ea31d087d96b30d38a3be47f0664d315a53517c
amir-shahsahebi <92686170+amir-shahsahebi@users.noreply.github.com>
2022-01-17 20:57:05 +0330

    an bug acurred

--- seperate&RestOperator/t.js
+++ seperate&RestOperator/t.js
@@ -1,16 +1,26 @@
 // ...
+console.log(null===false)
+let hdd = 4
 let obj = {
     "name" : "PC",
     "age" : 58,
     "color" : "black",
     "price value" : "$100",
-    hdd : "2tb",
-    count : x=> {
-        let count
-        count=count+x.map(y=>y)
+    hdd : 2,
+    count :  () {
+        let count=0
+         x.map(y=>count= y + count)
+        return count+ this.hdd
+    },
+    count2 : x=> {
+        let count=0
+         x.map(y=>count= y + count)
+        return count+ this.hdd
     }
 }
 
+// let arr = [1,2,3,4,5,6,7,8,9]
+// console.log(obj.count(arr))
 
 // let obj2 = {
 //     name : "PC",
@@ -18,11 +28,10 @@ let obj = {
 //     color : "black",
 //     price : "$100",
 // }
-let arr = [1,2,3,4,5,6,7,8,9]
-console.log(obj)
-console.log(arr)
-console.log(...arr)
-let { name, age, color, pricevalue, ...a } = obj;
-console.log(a)
+// console.log(obj)
+// console.log(arr)
+// console.log(...arr)
+// let { name, age, color, pricevalue, ...a } = obj;
+// console.log(a)
 // let {name , age , color , price} = obj2
 // console.log(name)
