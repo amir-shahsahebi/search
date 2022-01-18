b62f8430797bbb33ccfa2eaf466865080e59762d
amir shahsahebi <shahsahebiamir@gmail.com>
2022-01-18 13:33:09 +0330

    this dosent call in arrow function in obj of obj

--- seperate&RestOperator/testObj.js
+++ seperate&RestOperator/testObj.js
@@ -1,13 +1,38 @@
 let a = "apple"
+let aaa = "Find me"
 let b = function () {
     return "hey"+ " " +this.color
 }
+let c = () => {
+    return "Hi" + " " + this.rrr
+        } 
 let obj = {
     color : "red",
     size : "small",
-    a, 
-    b
+    rrr: "pink",
+    a , 
+    b ,
+    we : {c},
+    xc : {
+        ccc : () => {
+            return this
+        },
+        ttt : function () {
+            return this
+        },
+    },
+    www: function () {
+        let va=() => {
+            return this
+        } 
+        return va()
+    }
 }
-console.log(obj)
-console.log(obj.b())
-console.log(b())
+// console.log(obj)
+// console.log(obj.b())
+// console.log(b())
+// console.log(c())
+// console.log(obj.we.c())
+// console.log(obj.xc.ccc())
+// console.log(obj.xc.ttt())
+console.log(obj.www())
