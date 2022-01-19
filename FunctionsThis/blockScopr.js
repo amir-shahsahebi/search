c4c5c3d2f231d6ac9c97207bb7afeca5832f1e81
amir shahsahebi <shahsahebiamir@gmail.com>
2022-01-19 10:13:21 +0330

    block scope ended

--- FunctionsThis/blockScopr.js
+++ FunctionsThis/blockScopr.js
@@ -1,17 +1,17 @@
-let radius = 8
+// let radius = 8
 
-if (radius > 0 ) {
-  var PI = 3.14
-  let msg = "Hi"
-  console.log(msg)
-// BLOCK SCOPE
-}
+// if (radius > 0 ) {
+//   var PI = 3.14
+//   let msg = "Hi"
+//   console.log(msg)
+// // BLOCK SCOPE
+// }
 
-console.log(radius)
+// console.log(radius)
 
 
-console.log(PI)
-console.log(msg)
+// console.log(PI)
+// console.log(msg)
 
 
 
@@ -25,8 +25,8 @@ console.log(msg)
 
 for (let i = 0; i < 5; i++) {
   // BLOCK SCOPE
-  // let msg = "jasem is not a name, it is a culture.(benyamin(ع))"
-  // console.log(msg)
+  let msg = "jasem is not a name, it is a culture.(benyamin(ع))"
+  console.log(msg)
 }
 
 // console.log('this is outside the block scope:' , msg)
