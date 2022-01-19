2c1f32931f808d4d4cf06c58c312c11fa8301aa2
amir shahsahebi <shahsahebiamir@gmail.com>
2022-01-19 12:54:07 +0330

    seTime out completed

---
+++ FunctionsThis/setTimeOutThis.js
@@ -0,0 +1,24 @@
+// let tm= setTimeout(() => {
+//     console.log("Hello")
+//     console.log(this)
+// }, 1000);
+let obj = {
+    text: "Amir",
+    tm2 : function (){
+        setTimeout(() => {
+        console.log(this)
+        console.log("hello Im in Obj in arrow func form")
+    }, 1000)
+    },
+
+    tm3 : function () {
+        setTimeout(function () {
+        console.log(this)
+        console.log("a hello Im in Obj in function declaration form")
+    }, 1000)
+    },
+    
+
+}
+obj.tm2()
+obj.tm3()
