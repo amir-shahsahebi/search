aab10173ac4e4fc094032b0d419b74c1c690cdcf
amir shahsahebi <shahsahebiamir@gmail.com>
2022-02-01 23:29:22 +0330

    e.value.target added

--- AddEventListener/evet.js
+++ AddEventListener/evet.js
@@ -6,3 +6,11 @@ const button = document.querySelector("button");
 button.addEventListener("click", function (e) {
   console.log(e);
 });
+const input = document.querySelector("input");
+
+input.addEventListener("keyup", (e) => {
+  console.log(e);
+    console.log(e.key);
+    console.log(e.code);
+  console.log(e.target.value);
+});
