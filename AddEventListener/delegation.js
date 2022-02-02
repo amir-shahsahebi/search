4898eb52825895cf553cf7baa54fda1aba56ed84
amir shahsahebi <shahsahebiamir@gmail.com>
2022-02-02 21:17:42 +0330

    use delegation for changes new inputs

--- AddEventListener/delegation.js
+++ AddEventListener/delegation.js
@@ -1,36 +1,42 @@
 let lis = document.querySelectorAll("li");
-for (let li of lis) {
-  li.addEventListener("click", () => {
-    li.remove();
-  });
-}
+// for (let li of lis) {
+//   li.addEventListener("click", () => {
+//     li.remove();
+//   });
+// }
 
 const tweetsContainer = document.querySelector("#tweets");
 const tweetForm = document.querySelector("#tweetForm");
 
-// tweetForm.addEventListener("submit", (e) => {
-//   e.preventDefault();
+tweetForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-//   const usernameInput = tweetForm.elements.username;
-//   const tweetInput = tweetForm.elements.tweet;
+  const usernameInput = tweetForm.elements.username;
+  const tweetInput = tweetForm.elements.tweet;
 
-//   addTweet(usernameInput.value, tweetInput.value);
+  addTweet(usernameInput.value, tweetInput.value);
 
-//   usernameInput.value = "";
-//   tweetInput.value = "";
-// });
+  usernameInput.value = "";
+  tweetInput.value = "";
+});
 
-// const addTweet = (username, tweet) => {
-//   const newTweet = document.createElement("li");
-//   const bTag = document.createElement("b");
-//   bTag.append(username);
-//   newTweet.append(bTag);
-//   newTweet.append(`- ${tweet}`);
-//   tweetsContainer.append(newTweet);
-// };
+const addTweet = (username, tweet) => {
+  const newTweet = document.createElement("li");
+  const bTag = document.createElement("b");
+  bTag.append(username);
+  newTweet.append(bTag);
+  newTweet.append(`- ${tweet}`);
+  tweetsContainer.append(newTweet);
+};
 
-// tweetsContainer.addEventListener("click", (e) => {
-//   e.target.nodeName === "LI" && e.target.remove();
+tweetsContainer.addEventListener("click", (e) => {
+    console.dir(e.target);
+    console.log(e.target.nodeName);
+  e.target.nodeName === "LI" && e.target.remove();
 
-//   //   e.target.matches('li') && e.target.remove()
-// });
+  //   e.target.matches('li') && e.target.remove()
+});
+
+window.addEventListener("click", (e)=> {
+    console.dir(e.target.nodeName)
+})
