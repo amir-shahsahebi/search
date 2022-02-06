b1e71f1655149cc78aaed5bad7d3c30d361b6208
amir shahsahebi <shahsahebiamir@gmail.com>
2022-02-06 13:27:47 +0330

    adding palets

--- paint-JS/script.js
+++ paint-JS/script.js
@@ -4,10 +4,33 @@ board.addEventListener("mousemove",(e)=>{
     // dot.className = "p1";
     dot.classList.add("p1");
     dot.style.top= e.clientY + "px";
-    // console.log(e.clientY);
     dot.style.left= e.clientX + "px";
-    // console.log(e.clientX);
     board.append(dot)
+})
+
+let rgb = () => {
+    let r = Math.floor(Math.random()*256)
+    let g = Math.floor(Math.random()*256)
+    let b = Math.floor(Math.random()*256)
+    return rgb(r,g,b)
+}
+let n = 20
+palet = document.querySelector(".palet")
+function makeColor() {
+    for (i=1 ; i<n ; i++) {
+       let div2 = document.createElement("div")
+        div2.classList.add("color")
+        div2.style.backgroundColor = rgb()
+        palet.append(div2)
+    }
+}
+makeColor()
+
+palet.addEventListener("click", (e)=>{
+    if (e.target.clientHeight===30) {
+        color = e.target.style.backgroundColor
+    }
+    console.dir(color)
 })
 
 
