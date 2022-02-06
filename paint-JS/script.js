c7b3d3ed8141349b52271b6cca0d53405997d541
amir shahsahebi <shahsahebiamir@gmail.com>
2022-02-06 12:22:01 +0330

    palet container added

--- paint-JS/script.js
+++ paint-JS/script.js
@@ -1,10 +1,13 @@
 let board = document.querySelector(".white-board")
 board.addEventListener("mousemove",(e)=>{
-    // console.log("hey")
     dot = document.createElement("div");
+    // dot.className = "p1";
     dot.classList.add("p1");
     dot.style.top= e.clientY + "px";
-    console.log(e.clientY);
+    // console.log(e.clientY);
     dot.style.left= e.clientX + "px";
-    console.log(e.clientX);
+    // console.log(e.clientX);
+    board.append(dot)
 })
+
+
