cb94a3a55266aa500502bf87e8f755a93ab76222
amir-shahsahebi <92686170+amir-shahsahebi@users.noreply.github.com>
2022-02-12 20:20:27 +0330

    useful form of async added

--- async/async.js
+++ async/async.js
@@ -21,15 +21,14 @@
 //   });
 
 
-
-  const delayedColorChange = (color, delay) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      document.body.style.backgroundColor = color;
-      resolve();
-    }, delay);
-  });
-};
+//   const delayedColorChange = (color, delay) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       document.body.style.backgroundColor = color;
+//       resolve();
+//     }, delay);
+//   });
+// };
 
 // this syntax won't wait for the previous functions
 // async function rainbow() {
@@ -40,9 +39,37 @@
 // rainbow();
 
 //await will wait for a promise
-async function rainbow2() {
-    await delayedColorChange("red", 1000);
-  delayedColorChange("orange", 1000);
-}
-rainbow2();
+// async function rainbow2() {
+//     await delayedColorChange("red", 1000);
+//   delayedColorChange("orange", 1000);
+// }
+// rainbow2();
 
+const fakeRequest = (url) => {
+  return new Promise((resolved, reject) => {
+    const delay = Math.random() * 4500 + 500;
+    console.log(delay)
+    setTimeout(() => {
+      if (delay > 3000) {
+        reject("connection timeout");
+      } else {
+        resolved(`here is your fake data: ${url}`);
+      }
+    });
+  });
+};
+
+
+async function makeTwoRequests() {
+  try {
+    let data1 = await fakeRequest("/page1");
+    console.log(data1);
+    let data2 = await fakeRequest("/page2");
+    console.log(data2);
+  } catch (err) {
+    console.log("caught an error!");
+    console.log("error is : ", err);
+  }
+}
+
+makeTwoRequests();
