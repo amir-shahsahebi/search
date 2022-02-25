c19a05b66f170e7b6bb2382e8610cd340f8f6c4c
amir shahsahebi <shahsahebiamir@gmail.com>
2022-02-25 23:31:50 +0330

    free code camp added

--- exp/noe.js
+++ exp/noe.js
@@ -15,4 +15,45 @@ function zeroArray(m, n) {
   return newArray;
 }
 let matrix = zeroArray(3, 2);
-console.log(matrix);
+// console.log(matrix);
+
+
+//How to upperCase first letter of string 
+// let aaa="ali"
+// aaa.charAt(0)="A"
+// console.log(aaa);
+// function titleCase(str) {
+//   return str.split(" ").map(x=>x.replace(x[0],x[0].toUpperCase()))
+//  }
+//  console.log(titleCase("I'm a little tea pot"))
+
+
+// function mutation(arr) {
+//   if(arr[0].includes(arr[1])){
+//     return true
+//   }  
+//   return false
+//  }
+ 
+//  console.log(mutation(["hello", "hey"]))
+//  console.log(mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]))
+
+
+
+function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
+
+function Dog() { }
+
+// Only change code below this line
+Dog.prototype = Object.create(Animal.prototype)
+Dog.prototype.constructor = Dog
+Dog.prototype.bark = function () {
+  console.log("woof!")
+}
+
+// Only change code above this line
+
+let beagle = new Dog();
+console.log("hi")
+console.log(beagle.bark())
