7275c7991c589d66ab30f2d22137af91907eea10
amir shahsahebi <shahsahebiamir@gmail.com>
2022-02-28 23:04:37 +0330

    how to calculate differents times in ms

--- time/time.js
+++ time/time.js
@@ -2,8 +2,15 @@ let time = new Date()
 console.log(time)
 console.dir(time)
 console.log(time.getDate())
-console.log(time.getTime())
 console.log(time.toTimeString())
-console.log(time.toLocaleString())
 console.log(time.toDateString())
 console.log(time.toString())
+
+console.log(time.toLocaleString())
+console.log(time.getTime())
+
+//use get time to calculate differences days baetweeen old time and now 
+const before =new Date ('February 1 2020 8:50:10')
+console.log(before.getTime())
+diff = time.getTime() - before.getTime()
+console.log(diff)
