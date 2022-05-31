5080ac51a1d4f38975a0f537d7c44f1b96e32e7b
amir shahsahebi <shahsahebiamir@gmail.com>
2022-05-31 23:12:24 +0430

    dont give me 5 solved

--- JS/counter.js
+++ JS/counter.js
@@ -1,14 +1,10 @@
 function dontGiveMeFive(start, end) {
   let numbs = [];
-  for (let i = 0; i <= end - start; i++) {
-    if ((i + start) % 5 !== 0) numbs = [...numbs, i + start];
-    if ((i + start) % 10 === 0) numbs = [...numbs, i + start];
+  for (let i = 0; i <= Math.abs(end - start); i++) {
+    if (!(i + start).toString().includes("5")) numbs = [...numbs, i + start];
   }
   return numbs.length;
 }
 
-// const dontGiveMeFive1 = (start, end) =>
-//   end - start - Math.floor((end - start) / 10);
-
-console.log(dontGiveMeFive(1, 9));
-console.log(dontGiveMeFive(4, 17));
+console.log(dontGiveMeFive(49, 65));
+// console.log(dontGiveMeFive(4, 17));
